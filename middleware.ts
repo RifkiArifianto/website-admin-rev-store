16e70681ce8ce5c869207363c1ed904906c2bb0d
RifkiArifianto <rifkiarifianto@students.amikom.ac.id>
2025-02-25 09:51:07 +0700

    Fix Clerk middleware configuration

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,6 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware({
-  publicRoutes: ["/api/:path*"],
-});
+export default clerkMiddleware();
 
 export const config = {
   matcher: [
